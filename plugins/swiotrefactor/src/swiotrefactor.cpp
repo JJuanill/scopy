fc6ddf4a703c8b52aed8f31dd3b72574a71d878b
andrei.danila <andrei.danila@analog.com>
2024-04-09 13:47:09 +0300

    plugins/swiotrefactor: Import all functionalities from old swiot.
    Signed-off-by: andrei.danila <andrei.danila@analog.com>

--- plugins/swiotrefactor/src/swiotrefactor.cpp
+++ plugins/swiotrefactor/src/swiotrefactor.cpp
@@ -3,20 +3,50 @@
 #include <QLoggingCategory>
 #include <QLabel>
 
+#include "swiot_logging_categories.h"
 #include "max14906/max14906.h"
 #include "faults/faults.h"
+#include "ad74413r/ad74413r.h"
 
-#include <ad74413r/ad74413r.h>
+#include <iioutil/connectionprovider.h>
+
+#include <src/config/swiotconfig.h>
+
+#include <pluginbase/statusbarmanager.h>
 
 Q_LOGGING_CATEGORY(CAT_SWIOTREFACTOR, "SWIOTREFACTORPlugin")
 using namespace scopy::swiotrefactor;
 
 bool SWIOTREFACTORPlugin::compatible(QString m_param, QString category)
 {
-	// This function defines the characteristics according to which the
-	// plugin is compatible with a specific device
-	bool ret = true;
+	m_name = "SWIOT1L";
+	bool ret = false;
+	Connection *conn = ConnectionProvider::open(m_param);
+
+	if(!conn) {
+		qWarning(CAT_SWIOT) << "No context available for swiot";
+		return false;
+	}
+
+	iio_device *swiotDevice = iio_context_find_device(conn->context(), "swiot");
+	if(swiotDevice) {
+		ret = true;
+	}
+
+	ConnectionProvider::close(m_param);
+
 	return ret;
+}
+
+void SWIOTREFACTORPlugin::preload()
+{
+	m_displayName = "SWIOT1L";
+	m_swiotController = new SwiotController(m_param, this);
+	m_statusContainer = nullptr;
+	connect(m_swiotController, &SwiotController::contextSwitched, this, &SWIOTREFACTORPlugin::onCtxSwitched,
+		Qt::QueuedConnection);
+	connect(m_swiotController, &SwiotController::isRuntimeCtxChanged, this,
+		&SWIOTREFACTORPlugin::onIsRuntimeCtxChanged);
 }
 
 bool SWIOTREFACTORPlugin::loadPage()
@@ -50,79 +80,347 @@ bool SWIOTREFACTORPlugin::loadPage()
 	return true;
 }
 
+bool SWIOTREFACTORPlugin::loadExtraButtons()
+{
+	m_btnIdentify = new QPushButton("Identify");
+	m_extraButtons.append(m_btnIdentify);
+	connect(m_btnIdentify, SIGNAL(clicked()), m_swiotController, SLOT(identify()));
+
+	// The tutorial button will only be clickable when the user connects to the device
+	m_btnTutorial = new QPushButton(tr("Tutorial"));
+	m_extraButtons.append(m_btnTutorial);
+	m_btnTutorial->setEnabled(false);
+	m_btnTutorial->setToolTip("The tutorial will be available once the device is connected.");
+
+	return true;
+}
+
 bool SWIOTREFACTORPlugin::loadIcon()
 {
-	SCOPY_PLUGIN_ICON(":/gui/icons/adalm.svg");
+	m_icon = new QLabel("");
+	m_icon->setStyleSheet("border-image: url(:/swiotrefactor/swiot_icon.svg);");
 	return true;
 }
 
 void SWIOTREFACTORPlugin::loadToolList()
 {
 	m_toolList.append(
-		SCOPY_NEW_TOOLMENUENTRY("max14906", "Max14906", ":/gui/icons/scopy-default/icons/gear_wheel.svg"));
+		SCOPY_NEW_TOOLMENUENTRY(CONFIG_TME_ID, "Config", ":/gui/icons/scopy-default/icons/tool_debugger.svg"));
+	m_toolList.append(SCOPY_NEW_TOOLMENUENTRY(AD74413R_TME_ID, "AD74413R",
+						  ":/gui/icons/scopy-default/icons/tool_oscilloscope.svg"));
 	m_toolList.append(
-		SCOPY_NEW_TOOLMENUENTRY("faults", "Faults", ":/gui/icons/scopy-default/icons/gear_wheel.svg"));
-	m_toolList.append(
-		SCOPY_NEW_TOOLMENUENTRY("ad74413r", "Ad74413r", ":/gui/icons/scopy-default/icons/gear_wheel.svg"));
+		SCOPY_NEW_TOOLMENUENTRY(MAX14906_TME_ID, "MAX14906", ":/gui/icons/scopy-default/icons/tool_io.svg"));
+	m_toolList.append(SCOPY_NEW_TOOLMENUENTRY(FAULTS_TME_ID, "Faults", ":/swiotrefactor/tool_faults.svg"));
 }
 
 void SWIOTREFACTORPlugin::unload()
-{ /*delete m_infoPage;*/
-}
+{
+	disconnect(m_swiotController, &SwiotController::isRuntimeCtxChanged, this,
+		   &SWIOTREFACTORPlugin::onIsRuntimeCtxChanged);
+	disconnect(m_swiotController, &SwiotController::contextSwitched, this, &SWIOTREFACTORPlugin::onCtxSwitched);
 
-QString SWIOTREFACTORPlugin::description() { return "Write the plugin description here"; }
+	delete m_swiotController;
+}
 
 bool SWIOTREFACTORPlugin::onConnect()
 {
-	// This method is called when you try to connect to a device and the plugin is
-	// compatible to that device
-	// In case of success the function must return true and false otherwise
 
-	Max14906 *max14906 = new Max14906(m_param, m_toolList[0]);
-	m_toolList[0]->setTool(max14906);
-	m_toolList[0]->setEnabled(true);
-	m_toolList[0]->setRunBtnVisible(true);
+	Connection *conn = ConnectionProvider::open(m_param);
+	if(!conn) {
+		return false;
+	}
 
-	Faults *faults = new Faults(m_param, m_toolList[1]);
-	m_toolList[1]->setTool(faults);
-	m_toolList[1]->setEnabled(true);
-	m_toolList[1]->setRunBtnVisible(true);
+	m_runtime = new SwiotRuntime(m_param, this);
+	connect(m_runtime, &SwiotRuntime::writeModeAttribute, m_swiotController, &SwiotController::writeModeAttribute);
+	connect(m_swiotController, &SwiotController::modeAttributeChanged, m_runtime,
+		&SwiotRuntime::modeAttributeChanged);
+	connect(m_runtime, &SwiotRuntime::backBtnPressed, this, &SWIOTREFACTORPlugin::startCtxSwitch);
+	connect(m_swiotController, &SwiotController::isRuntimeCtxChanged, m_runtime,
+		&SwiotRuntime::onIsRuntimeCtxChanged);
+	connect(m_swiotController, &SwiotController::hasConnectedPowerSupply, this,
+		&SWIOTREFACTORPlugin::powerSupplyStatus);
 
-	Ad74413r *ad74413r = new Ad74413r(m_param, m_toolList[2]);
-	m_toolList[2]->setTool(ad74413r);
-	m_toolList[2]->setEnabled(true);
-	m_toolList[2]->setRunBtnVisible(true);
+	m_swiotController->connectSwiot();
+	m_swiotController->startPingTask();
+	m_swiotController->startPowerSupplyTask("ext_psu");
+	m_btnTutorial->setEnabled(true);
+	m_btnTutorial->setToolTip("");
+
+	return true;
 
 	return true;
 }
 
 bool SWIOTREFACTORPlugin::onDisconnect()
 {
-	// This method is called when the disconnect button is pressed
-	// It must remove all connections that were established on the connection
-	for(auto &tool : m_toolList) {
-		tool->setEnabled(false);
-		tool->setRunning(false);
-		tool->setRunBtnVisible(false);
-		QWidget *w = tool->tool();
-		if(w) {
-			tool->setTool(nullptr);
-			delete(w);
-		}
+	auto configTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, CONFIG_TME_ID);
+	auto ad74413rTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, AD74413R_TME_ID);
+	auto max14906Tme = ToolMenuEntry::findToolMenuEntryById(m_toolList, MAX14906_TME_ID);
+	auto faultsTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, FAULTS_TME_ID);
+
+	for(ToolMenuEntry *tme : qAsConst(m_toolList)) {
+		tme->setRunning(false);
+		tme->setEnabled(false);
+		tme->setRunBtnVisible(false);
+		tme->tool()->deleteLater();
+		tme->setTool(nullptr);
 	}
+
+	disconnect(dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::configBtnPressed, this,
+		   &SWIOTREFACTORPlugin::startCtxSwitch);
+	disconnect(m_swiotController, &SwiotController::modeAttributeChanged,
+		   dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::modeAttributeChanged);
+	disconnect(dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::writeModeAttribute, m_swiotController,
+		   &SwiotController::writeModeAttribute);
+	disconnect(dynamic_cast<Ad74413r *>(ad74413rTme->tool()), &Ad74413r::configBtnPressed, m_runtime,
+		   &SwiotRuntime::onBackBtnPressed);
+	disconnect(dynamic_cast<Max14906 *>(max14906Tme->tool()), &Max14906::configBtnPressed, m_runtime,
+		   &SwiotRuntime::onBackBtnPressed);
+	disconnect(dynamic_cast<Faults *>(faultsTme->tool()), &Faults::backBtnPressed, m_runtime,
+		   &SwiotRuntime::onBackBtnPressed);
+
+	disconnect(m_swiotController, &SwiotController::hasConnectedPowerSupply, this,
+		   &SWIOTREFACTORPlugin::powerSupplyStatus);
+
+	disconnect(m_swiotController, &SwiotController::pingFailed, this, &SWIOTREFACTORPlugin::disconnectDevice);
+
+	disconnect(m_swiotController, &SwiotController::isRuntimeCtxChanged, m_runtime,
+		   &SwiotRuntime::onIsRuntimeCtxChanged);
+	disconnect(m_runtime, &SwiotRuntime::backBtnPressed, this, &SWIOTREFACTORPlugin::startCtxSwitch);
+	disconnect(m_swiotController, &SwiotController::modeAttributeChanged, m_runtime,
+		   &SwiotRuntime::modeAttributeChanged);
+	disconnect(m_runtime, &SwiotRuntime::writeModeAttribute, m_swiotController,
+		   &SwiotController::writeModeAttribute);
+
+	m_swiotController->stopPingTask();
+	m_swiotController->stopPowerSupplyTask();
+	m_swiotController->stopTemperatureTask();
+	m_swiotController->disconnectSwiot();
+
+	m_btnTutorial->setEnabled(false);
+
+	if(m_runtime) {
+		delete m_runtime;
+		m_runtime = nullptr;
+	}
+
+	if(m_statusContainer) {
+		delete m_statusContainer;
+		m_statusContainer = nullptr;
+	}
+
+	ConnectionProvider::close(m_param);
 	return true;
 }
+
+void SWIOTREFACTORPlugin::startCtxSwitch()
+{
+	m_swiotController->stopPingTask();
+	m_swiotController->stopPowerSupplyTask();
+	//	if (m_isRuntime) {
+	//		m_swiotController->stopTemperatureTask();
+	//	}
+
+	m_swiotController->disconnectSwiot();
+	Q_EMIT disconnectDevice();
+	m_swiotController->startSwitchContextTask(m_isRuntime);
+}
+
+void SWIOTREFACTORPlugin::onCtxSwitched()
+{
+	m_swiotController->stopSwitchContextTask();
+	Q_EMIT connectDevice();
+}
+
+void SWIOTREFACTORPlugin::onIsRuntimeCtxChanged(bool isRuntimeCtx)
+{
+	m_isRuntime = isRuntimeCtx;
+	setupToolList();
+}
+
+void SWIOTREFACTORPlugin::startTutorial()
+{
+	if(m_isRuntime) {
+		qInfo(CAT_SWIOT) << "Starting SWIOT runtime tutorial.";
+
+		// The tutorial builder is responsible for deleting itself after the tutorial is finished, so creating
+		// a new one each time the tutorial is started will not create memory leaks.
+		ToolMenuEntry *ad74413rTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, AD74413R_TME_ID);
+		ToolMenuEntry *max14906Tme = ToolMenuEntry::findToolMenuEntryById(m_toolList, MAX14906_TME_ID);
+		ToolMenuEntry *faultsTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, FAULTS_TME_ID);
+
+		QWidget *ad74413rTool = ad74413rTme->tool();
+		QWidget *max14906Tool = max14906Tme->tool();
+		QWidget *faultsTool = faultsTme->tool();
+		QWidget *parent = Util::findContainingWindow(ad74413rTool);
+
+		m_ad74413rTutorial =
+			new gui::TutorialBuilder(ad74413rTool, ":/swiot/tutorial_chapters.json", "ad74413r", parent);
+		m_max14906Tutorial =
+			new gui::TutorialBuilder(max14906Tool, ":/swiot/tutorial_chapters.json", "max14906", parent);
+		m_faultsTutorial =
+			new gui::TutorialBuilder(faultsTool, ":/swiot/tutorial_chapters.json", "faults", parent);
+
+		connect(m_ad74413rTutorial, &gui::TutorialBuilder::finished, this,
+			&SWIOTREFACTORPlugin::startMax14906Tutorial);
+		connect(m_max14906Tutorial, &gui::TutorialBuilder::finished, this,
+			&SWIOTREFACTORPlugin::startFaultsTutorial);
+		connect(m_ad74413rTutorial, &gui::TutorialBuilder::aborted, this, &SWIOTREFACTORPlugin::abortTutorial);
+
+		this->startAd74413rTutorial();
+	} else {
+		qInfo(CAT_SWIOT) << "Starting SWIOT config tutorial.";
+		auto configTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, CONFIG_TME_ID);
+
+		auto configTool = configTme->tool();
+		auto parent = Util::findContainingWindow(configTool);
+		auto tut = new gui::TutorialBuilder(configTool, ":/swiot/tutorial_chapters.json", "config", parent);
+
+		tut->setTitle("CONFIG");
+		requestTool(configTme->id());
+		tut->start();
+	}
+}
+
+void SWIOTREFACTORPlugin::startAd74413rTutorial()
+{
+	qInfo(CAT_SWIOT) << "Starting ad74413r tutorial.";
+	ToolMenuEntry *ad74413rTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, AD74413R_TME_ID);
+	this->requestTool(ad74413rTme->id());
+	m_ad74413rTutorial->setTitle("AD74413R");
+	m_ad74413rTutorial->start();
+}
+
+void SWIOTREFACTORPlugin::startMax14906Tutorial()
+{
+	qInfo(CAT_SWIOT) << "Starting max14906 tutorial.";
+	ToolMenuEntry *max14906Tme = ToolMenuEntry::findToolMenuEntryById(m_toolList, MAX14906_TME_ID);
+	this->requestTool(max14906Tme->id());
+	m_max14906Tutorial->setTitle("MAX14906");
+	m_max14906Tutorial->start();
+}
+
+void SWIOTREFACTORPlugin::startFaultsTutorial()
+{
+	qInfo(CAT_SWIOT) << "Starting faults tutorial.";
+	ToolMenuEntry *faultsTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, FAULTS_TME_ID);
+	this->requestTool(faultsTme->id());
+	m_faultsTutorial->setTitle("FAULTS");
+	m_faultsTutorial->start();
+}
+
+void SWIOTREFACTORPlugin::powerSupplyStatus(bool ps)
+{
+	if(!ps) {
+		if(!m_statusContainer) {
+			createStatusContainer();
+			StatusBarManager::pushWidget(m_statusContainer, "ExtPsuStatus");
+		}
+	} else {
+		if(m_statusContainer) {
+			delete m_statusContainer;
+			m_statusContainer = nullptr;
+		}
+	}
+}
+
+void SWIOTREFACTORPlugin::createStatusContainer()
+{
+	m_statusContainer = new QWidget();
+	m_statusContainer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+	m_statusContainer->setLayout(new QHBoxLayout(m_statusContainer));
+	m_statusContainer->layout()->setSpacing(0);
+	m_statusContainer->layout()->setContentsMargins(0, 0, 0, 0);
+
+	auto exclamationIcon = new QPushButton(m_statusContainer);
+	StyleHelper::NoBackgroundIconButton(exclamationIcon, QIcon::fromTheme(":/swiot/warning.svg"));
+
+	auto statusLabel = new QLabel("AD-SWIOT1L-SL: The system is powered at limited capacity.");
+	statusLabel->setWordWrap(true);
+	StyleHelper::WarningLabel(statusLabel, "extPsuStatusLabel");
+
+	m_statusContainer->layout()->addWidget(exclamationIcon);
+	m_statusContainer->layout()->addWidget(statusLabel);
+}
+
+void SWIOTREFACTORPlugin::setupToolList()
+{
+	m_swiotController->startTemperatureTask();
+
+	auto configTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, CONFIG_TME_ID);
+	auto ad74413rTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, AD74413R_TME_ID);
+	auto max14906Tme = ToolMenuEntry::findToolMenuEntryById(m_toolList, MAX14906_TME_ID);
+	auto faultsTme = ToolMenuEntry::findToolMenuEntryById(m_toolList, FAULTS_TME_ID);
+
+	for(ToolMenuEntry *tme : qAsConst(m_toolList)) {
+		tme->setEnabled(true);
+		tme->setVisible(true);
+		if(!m_isRuntime) {
+			if(tme->id().compare(CONFIG_TME_ID)) {
+				tme->setVisible(false);
+			}
+		} else {
+			if(tme->id().compare(CONFIG_TME_ID)) {
+				tme->setRunBtnVisible(true);
+				tme->setRunning(false);
+			} else {
+				tme->setVisible(false);
+			}
+		}
+	}
+
+	if(m_isRuntime) {
+		ad74413rTme->setTool(new swiotrefactor::Ad74413r(m_param, ad74413rTme));
+		max14906Tme->setTool(new swiotrefactor::Max14906(m_param, max14906Tme));
+		faultsTme->setTool(new swiotrefactor::Faults(m_param, faultsTme));
+	} else {
+		configTme->setTool(new swiotrefactor::SwiotConfig(m_param));
+	}
+
+	connect(dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::writeModeAttribute, m_swiotController,
+		&SwiotController::writeModeAttribute);
+	connect(m_swiotController, &SwiotController::modeAttributeChanged,
+		dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::modeAttributeChanged);
+	connect(dynamic_cast<SwiotConfig *>(configTme->tool()), &SwiotConfig::configBtnPressed, this,
+		&SWIOTREFACTORPlugin::startCtxSwitch);
+
+	connect(dynamic_cast<Ad74413r *>(ad74413rTme->tool()), &Ad74413r::configBtnPressed, m_runtime,
+		&SwiotRuntime::onBackBtnPressed);
+	connect(dynamic_cast<Max14906 *>(max14906Tme->tool()), &Max14906::configBtnPressed, m_runtime,
+		&SwiotRuntime::onBackBtnPressed);
+	connect(dynamic_cast<Faults *>(faultsTme->tool()), &Faults::backBtnPressed, m_runtime,
+		&SwiotRuntime::onBackBtnPressed);
+
+	connect(m_swiotController, &SwiotController::pingFailed, this, &SWIOTREFACTORPlugin::disconnectDevice);
+
+	if(!m_isRuntime) {
+		requestTool(configTme->id());
+	} else {
+		requestTool(ad74413rTme->id());
+	}
+}
+
+void SWIOTREFACTORPlugin::abortTutorial()
+{
+	disconnect(m_ad74413rTutorial, &gui::TutorialBuilder::finished, this,
+		   &SWIOTREFACTORPlugin::startMax14906Tutorial);
+	disconnect(m_max14906Tutorial, &gui::TutorialBuilder::finished, this,
+		   &SWIOTREFACTORPlugin::startFaultsTutorial);
+}
+
+QString SWIOTREFACTORPlugin::description() { return "Adds functionality specific to SWIOT1L board"; }
 
 void SWIOTREFACTORPlugin::initMetadata()
 {
 	loadMetadata(
 		R"plugin(
 	{
-	   "priority":110,
+	   "priority":100,
 	   "category":[
 	      "iio"
 	   ],
-	   "exclude":[""]
+	   "exclude":["*", "!debuggerplugin"],
+	   "include-forced":["regmapplugin"]
 	}
 )plugin");
 }
